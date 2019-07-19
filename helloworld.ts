4c18f825bab1a8dfa923f087d961632e6e8e2978
i070637 <eric.su@sap.com>
2019-07-19 18:23:29 +0800

    exercise 1 of basic knowledge

--- helloworld.ts
+++ helloworld.ts
@@ -1,12 +1,17 @@
 import _ = require("lodash");
  
+'use strict';
+
 let msg : string = "hello muggle!"
 console.log(msg);
+
+//guess what will print out,  1,2,3... or 10?
 
 for (let i = 0; i < 10; i++) {
     setTimeout(function() { console.log(i); }, 100 * i);
 }
 
+//correct version
 // for (var i = 0; i < 10; i++) {
 //     // capture the current state of 'i'
 //     // by invoking a function with its current value
@@ -15,6 +20,7 @@ for (let i = 0; i < 10; i++) {
 //     })(i);
 // }
 
+// optional member
 interface iPoint{
     x : number;
     y : number;
@@ -44,3 +50,109 @@ function insPoint(p:iPoint) : iPoint{
 let ins : (p: iPoint) => iPoint = insPoint;
 
 console.log(ins({x:1,y:2,z:4}));
+
+//interface
+interface Shape {
+    color: string;
+}
+
+let square = <Shape>{};
+square.color = 'blue';
+
+console.log(square.color);
+
+//class prototype
+class Greeter {
+    static standardGreeting = "Hello, there";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        }
+        else {
+            return Greeter.standardGreeting;
+        }
+    }
+}
+
+let greeter1: Greeter;
+greeter1 = new Greeter();
+console.log(greeter1.greet());
+
+let greeterMaker: typeof Greeter = Greeter;
+greeterMaker.standardGreeting = "Hey there!";
+
+let greeter2: Greeter = new greeterMaker();
+console.log(greeter2.greet());
+
+
+// type emit
+// myAdd has the full function type
+let myAdd1 = function(x: number, y: number): number { return x + y; };
+
+// The parameters `x` and `y` have the type number
+let myAdd2: (baseValue: number, increment: number) => number =
+    function(x, y) { return x + y; };
+
+let z = myAdd1(1,2);
+z = myAdd2(2,3);
+
+
+// this pointer
+let deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function() {
+        // NOTE: the line below is now an arrow function, allowing us to capture 'this' right here
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker(); // why no error throw here?
+
+console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+
+// function loggingIdentity<T>(arg: T): T {
+//     console.log(arg.length);  // Error: T doesn't have .length
+//     return arg;
+// }
+
+interface Lengthwise {
+    length: number;
+}
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length);  // Now we know it has a .length property, so no more error
+    return arg;
+}
+
+loggingIdentity({length: 10, value: 3});
+//loggingIdentity(3); //error while compile
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+let x = { a: 1, b: 2, c: 3, d: 4 };
+getProperty(x, "a"); // okay
+//getProperty(x, "m"); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.
+
+// type compatible
+interface Named {
+    name: string;
+}
+
+class Person {
+    name: string;
+}
+
+let p: Named;
+// OK, because of structural typing although Person doesn't implement Named
+p = new Person();
+
+console.log('end');
